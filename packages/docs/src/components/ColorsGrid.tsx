ea76ea68573f046120dca60f0b5ad2d6ddc298f7
mateus-cristian <mateus_cristian@outlook.com>
2023-06-05 22:57:31 -0300

    feat:components tokens

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,5 +1,6 @@
-import { colors } from '@redshift-ui/tokens'
 import React from 'react'
+import { colors } from '@redshift-ui/tokens'
+import { getContrast } from 'polished'
 
 export default function ColorsGrid() {
     return Object.entries(colors).map(([key, color]) => {
@@ -9,7 +10,7 @@ export default function ColorsGrid() {
                     display: "flex",
                     justifyContent: 'space-between',
                     fontFamily: 'monospace',
-                    color: "#fff"
+                    color: getContrast(color, "#fff") < 3.5 ? "#222" : "#fff"
                 }}>
                     <strong>${key}</strong>
                     <strong>{color}</strong>
---
+++ packages/docs/src/components/TokensGrid.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../styles/tokens-grid.css'
+
+interface TokenGridProps {
+    tokens: Record<string, string>
+    hasRemValue?: boolean;
+}
+
+export default function TokensGrid({ tokens, hasRemValue = false }: TokenGridProps) {
+    return (
+        <table className='tokens-grid'>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Value</th>
+                    {hasRemValue && <th>Pixels</th>}
+                </tr>
+            </thead>
+
+            <tbody>
+                {Object.entries(tokens).map(([key, value]) => {
+                    return (
+                        <tr key={key}>
+                            <td>{key}</td>
+                            <td>{value}</td>
+                            {hasRemValue && (
+                                <td>{Number(value.replace('rem', '')) * 16}px</td>
+                            )}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+    )
+}
+
