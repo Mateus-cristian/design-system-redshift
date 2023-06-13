6b60eefdf5115b41939c8ebf02385c35e17b5250
mateus-cristian <mateus_cristian@outlook.com>
2023-06-13 20:33:56 -0300

    feat:update config

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -5,13 +5,15 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-docs",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
+    docsMode: true,
   },
 };
 export default config;
