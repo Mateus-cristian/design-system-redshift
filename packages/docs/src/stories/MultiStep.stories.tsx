bf4409a9f195552a1a6de0279f59df1acf76e07c
mateus-cristian <mateus_cristian@outlook.com>
2023-06-13 20:33:31 -0300

    feat:component multistep

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,36 @@
+import type { StoryObj, Meta } from "@storybook/react"
+import { MultiStepProps, MultiStep, Box, Text } from '@redshift-ui/react'
+
+export default {
+    title: 'Form/MultiStep',
+    component: MultiStep,
+    args: {
+        size: 4,
+        currentStep: 1
+    },
+    decorators: [
+        (Story) => {
+            return (
+                <Box as="label" css={{ display: 'flex', flexDirection: 'column', gap: "$2" }}>
+                    {Story()}
+                </Box>
+            )
+        }
+    ]
+
+} as Meta<MultiStepProps>
+
+
+
+export const Primary: StoryObj<MultiStepProps> = {
+
+}
+
+export const Full: StoryObj<MultiStepProps> = {
+    args: {
+        currentStep: 4
+    }
+}
+
+
+
---
+++ packages/react/src/components/MultiStep/styles.ts
@@ -0,0 +1,34 @@
+import { styled } from "../../styles";
+import { Text } from "../Text";
+
+export const MultiStepContainer = styled("div", {});
+
+export const Label = styled(Text, {
+  color: "$gray100",
+  margin: "0",
+
+  defaultVariants: {
+    size: "$sx",
+  },
+});
+
+export const Steps = styled("div", {
+  display: "grid",
+  gridTemplateColumns: "repeat(var(--steps-size), 1fr)",
+  gap: "$2",
+  marginTop: "$1",
+});
+
+export const Step = styled("div", {
+  height: "$1",
+  borderRadius: "$px",
+  backgroundColor: "$gray600",
+
+  variants: {
+    active: {
+      true: {
+        backgroundColor: "$gray100",
+      },
+    },
+  },
+});
