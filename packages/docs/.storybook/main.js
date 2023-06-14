f51ecd3f0f2b2bda7384d16f0804b9bf32871b62
mateus-cristian <mateus_cristian@outlook.com>
2023-06-13 23:10:47 -0300

    fix:main

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-docs",
-    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
