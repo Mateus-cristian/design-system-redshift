3bcdf3e1f45d64682de44ebee0471388ef378158
mateus-cristian <mateus_cristian@outlook.com>
2023-06-13 22:07:19 -0300

    fix:name app

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/05-design-system/";
+      config.base = "/design-system-redshift/";
     }
 
     return config;
