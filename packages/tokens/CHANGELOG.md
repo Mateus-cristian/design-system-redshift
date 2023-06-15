f31f76ce7d7cfe8a8668a802dd0def65adb3149f
mateus-cristian <mateus_cristian@outlook.com>
2023-06-15 20:53:55 -0300

    feat:add task new color

---
+++ packages/tokens/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @redshiftui/tokens
+
+## 1.1.0
+
+### Minor Changes
+
+- add color red for test
--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -2,6 +2,7 @@
 export const colors = {
   white: "#FFF",
   black: "#000",
+  red: "red",
 
   gray100: "#E1E1E6",
   gray200: "#A9A9B2",
